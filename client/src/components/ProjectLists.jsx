6ed723045081ced775fc38efc638c537bdb4cfbb
ImeshaDilshani <imeshadilshani212@gmail.com>
2024-06-16 14:55:28 +0530

    change

--- client/src/components/ProjectLists.jsx
+++ client/src/components/ProjectLists.jsx
@@ -1,47 +1,46 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getListProjects } from '../actions/projectAction';
-import { Col, Card, Container, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-
+import { Col, Card, Container, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const ProjectLists = () => {
-  // Get data from store
   const dispatch = useDispatch();
   const projectLists = useSelector(state => state.projectLists);
-  const {success, loading, projects} = projectLists;
-
-  console.log(projects);
+  const { loading, projects } = projectLists;
 
   useEffect(() => {
     dispatch(getListProjects());
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>all Projects List</h1>
+    <Container className="mt-4">
+      <h1 className="mb-4">Project Tracker</h1>
       <hr />
-      <Container>
-      <Row>
-        {projects.map((projects) =>(
-          <Col xs={12} md={6} lg={4} key={projects.id}>
-            <Card style = {{width:'18rem'}} className='m-3 pd-2'>
-            <Card.Img variant='top' height='150px' src={projects.thumbnail}/>
-            <Card.Body>
-              <Card.Title>{projects.title}</Card.Title>
-              <Card.Text><b>({projects.category})</b></Card.Text>
-              <hr/>
-              <span><a className='btn btn-info m-2' href={projects.demo} target = '_blank' rel="noreferrer">Demo</a>
-              {/* <Link style={{textDecoration:'none', marginLeft:'2px', marginRight:'2px'}} className='m-2 pd-2 btn btn-success'>View</Link> */}
-              </span>
-            </Card.Body>
-            </Card>
-            
-          </Col>
-        ))}
-      </Row>
-      </Container>
-    </div>
+
+      {loading ? (
+        <p>Loading projects...</p>
+      ) : (
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {projects.map(project => (
+            <Col key={project.id}>
+              <Card className="h-100">
+                <Card.Img variant="top" src={project.thumbnail} className="card-img-top" />
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text className="mb-2 text-muted"><b>({project.category})</b></Card.Text>
+                  <Card.Text>{project.description}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <Button variant="info" className="me-2" href={project.demo} target="_blank" rel="noreferrer">Demo</Button>
+                  <Link to={`/project/${project.id}`} className="btn btn-success">View Details</Link>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      )}
+    </Container>
   );
 };
 
